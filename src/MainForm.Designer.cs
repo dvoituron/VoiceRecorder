2e5e392c0d7fe442852ce507f575c778f93f890e
Christophe Peugnet <christophe.peugnet@outlook.fr>
2019-08-31 15:52:16 +0200

    - choice of recording path - at the end of the recording, the folder opens

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -35,6 +35,9 @@
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.lblTime = new System.Windows.Forms.Label();
             this.lblStatus = new System.Windows.Forms.Label();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
             // 
             // panDevices
@@ -42,9 +45,9 @@
             this.panDevices.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panDevices.Location = new System.Drawing.Point(12, 87);
+            this.panDevices.Location = new System.Drawing.Point(12, 114);
             this.panDevices.Name = "panDevices";
-            this.panDevices.Size = new System.Drawing.Size(304, 227);
+            this.panDevices.Size = new System.Drawing.Size(304, 200);
             this.panDevices.TabIndex = 2;
             // 
             // btnRecord
@@ -54,7 +57,7 @@
             this.btnRecord.ImageList = this.imageList1;
             this.btnRecord.Location = new System.Drawing.Point(12, 12);
             this.btnRecord.Name = "btnRecord";
-            this.btnRecord.Size = new System.Drawing.Size(163, 69);
+            this.btnRecord.Size = new System.Drawing.Size(159, 68);
             this.btnRecord.TabIndex = 3;
             this.btnRecord.UseVisualStyleBackColor = false;
             this.btnRecord.Click += new System.EventHandler(this.BtnRecord_Click);
@@ -64,7 +67,7 @@
             this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             this.imageList1.Images.SetKeyName(0, "Record.png");
-            this.imageList1.Images.SetKeyName(1, "Pause.png");
+            this.imageList1.Images.SetKeyName(1, "stop.png");
             // 
             // lblTime
             // 
@@ -85,11 +88,30 @@
             this.lblStatus.TabIndex = 5;
             this.lblStatus.Text = "Not recording";
             // 
+            // btnPath
+            // 
+            this.btnPath.Image = ((System.Drawing.Image)(resources.GetObject("btnPath.Image")));
+            this.btnPath.Location = new System.Drawing.Point(284, 82);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(28, 26);
+            this.btnPath.TabIndex = 6;
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 86);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(266, 20);
+            this.txtPath.TabIndex = 7;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(324, 328);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnPath);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.lblTime);
             this.Controls.Add(this.btnRecord);
@@ -111,6 +133,9 @@
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.Label lblTime;
         private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
